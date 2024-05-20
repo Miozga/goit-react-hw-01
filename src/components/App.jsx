18ecfbcc8ffa1941467ed9a3c6e0c9c6e6702363
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-05-20 16:20:10 +0200

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,16 @@
-import Profile from './Profile/Profile';
-import user from './Profile/user.json';
+import Profile from './components/Profile/Profile';
+import user from './path/to/user.json';
 
-import data from './Statistics/data.json';
-import Statistics from './Statistics/Statistics';
+const App = () => (
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
+);
 
-import FriendList from './FriendList/FriendList';
-import friends from './FriendList/friends.json';
-
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from './TransactionHistory/transactions.json';
-
-export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />;
-    </div>
-  );
-};
+export default App;
